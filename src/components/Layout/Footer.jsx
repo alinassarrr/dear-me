40be79fac2e5f40fcf04420580474ed7f284bf23
alinassarrr <aliinassar8@gmail.com>
2025-07-12 19:55:49 +0300

    Create footer component

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="container">
+      <h2>DearMe</h2>
+      <p className="text">
+        Send messages to your future self. Reflect on your journey when its time
+        come.
+      </p>
+      <p className="rights">
+        © 2025 DearMe • All rights reserved • By Ali Nassar
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
