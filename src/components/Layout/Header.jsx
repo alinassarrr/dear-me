98d7484430e87a81853ce6f231b53ee2987c371d
alinassarrr <aliinassar8@gmail.com>
2025-07-13 03:34:45 +0300

    Create nav buttons and assigning active className to home by default

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,31 +1,35 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
-import Login from "../../pages/Login";
 const Header = () => {
   const loggedIn = false;
+  const buttons = [
+    { text: "Home", to: "/", id: "home" },
+    { text: "Create", to: "/create", id: "create" },
+    { text: "Public Wall", to: "/public", id: "public" },
+    loggedIn
+      ? { text: "Profile", to: "/profile", id: "profile" }
+      : { text: "SignUp", to: "/login", id: "login" },
+  ];
   return (
     <header className="container">
       <div className="logo">DearMe</div>
       <nav>
         <ul>
-          <li>
-            <Link className="active" to={"/"}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link>Create</Link>
-          </li>
-          <li>
-            <Link>Pulic Wall</Link>
-          </li>
-          <li>
-            {loggedIn ? (
-              <Link>Profile</Link>
-            ) : (
-              <Link to={"/login"}>SignUp</Link>
-            )}
-          </li>
+          {buttons.map((btn) => {
+            return (
+              <li key={btn.id}>
+                {btn.id === "home" ? (
+                  <Link id={btn.id} className="nav-btn active" to={btn.to}>
+                    {btn.text}
+                  </Link>
+                ) : (
+                  <Link id={btn.id} className="nav-btn" to={btn.to}>
+                    {btn.text}
+                  </Link>
+                )}
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
