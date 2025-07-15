1a77dc4d4e4064edb254cdb8a9f50e0f2bb621aa
alinassarrr <aliinassar8@gmail.com>
2025-07-15 22:42:06 +0300

    Update changes of path for components

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import Login from "../pages/Login";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 import Public from "../pages/Public";
-import Create from "../pages/Create";
+import Create from "../pages/Create/Create";
 import Profile from "../pages/Profile";
 
 const Routing = () => {
