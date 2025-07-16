4666b83509d1894e669ba05b2ddef40a36781bcc
alinassarrr <aliinassar8@gmail.com>
2025-07-16 17:32:42 +0300

    Fix the map of key error

--- src/components/Common/Dropdown/Dropdown.jsx
+++ src/components/Common/Dropdown/Dropdown.jsx
@@ -3,8 +3,10 @@ const Dropdown = (props) => {
   const { id, onChange, value, options } = props;
   return (
     <select id={id} onChange={onChange} value={value}>
-      {options.map((option) => (
-        <option value={option.value}>{option.text}</option>
+      {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.text}
+        </option>
       ))}
     </select>
   );
