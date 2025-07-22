032bce32946a94e3a9a52ee6ed48df15e3c91c4f
alinassarrr <aliinassar8@gmail.com>
2025-07-23 00:06:44 +0300

    Create custom hook for profile page

---
+++ src/pages/Profile/ProfileLogic.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useAuthApi } from "../../hooks/custom/AuthApiCall";
+import axios from "axios";
+
+export const useProfileLogic = () => {
+  const [path, token, navigate, user] = useAuthApi();
+
+  const username = user.username;
+  const email = user.email;
+  const [capsules, setCapsules] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [revealed, setRevealed] = useState(0);
+  const [waiting, setWaiting] = useState(0);
+
+  useEffect(() => {
+    userCapsules();
+  }, []);
+
+  const userCapsules = async () => {
+    try {
+      const response = await axios.get(`${path}/profile/my-capsules`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const allCaps = response.data.data;
+      setCapsules(allCaps);
+      setTotal(allCaps.length);
+      const revealed = allCaps.filter((cap) => cap.revealed);
+      // console.log(revealed);
+      setRevealed(revealed.length);
+      setWaiting(allCaps.length - revealed.length);
+    } catch (error) {
+      console.log("Error fetching capsules:", error);
+    }
+  };
+
+  return [username, email, capsules, total, revealed, waiting];
+};
