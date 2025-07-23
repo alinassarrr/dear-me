abfafaa5688e52a3af3b6db6b72dc1e06771494b
alinassarrr <aliinassar8@gmail.com>
2025-07-23 18:43:37 +0300

    Check on each page if user loggedIn or not

--- src/hooks/custom/AuthApiCall.js
+++ src/hooks/custom/AuthApiCall.js
@@ -1,11 +1,16 @@
-import React from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const useAuthApi = () => {
   const path = import.meta.env.VITE_BASE_URL;
   const user = JSON.parse(localStorage.getItem("user"));
-  const token = user.token;
+  const token = user?.token;
   const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      navigate("/signup");
+    }
+  }, [token, navigate]);
 
   return [path, token, navigate, user];
 };
